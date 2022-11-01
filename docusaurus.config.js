bb558da3a34ab46b1dddb35c393fda92bb1b9653
Ryan Hay <58194660+ryan-hay@users.noreply.github.com>
2022-11-01 20:29:30 +1000

    analytics (#100)
    Signed-off-by: Ryan Hay <ryanhay@au.ibm.com>
    
    Signed-off-by: Ryan Hay <ryanhay@au.ibm.com>
    Co-authored-by: Ryan Hay <ryanhay@au.ibm.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -52,6 +52,10 @@ const config = {
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
         },
+        gtag: {
+          trackingID: "G-7WTTHT6SEH",
+          anonymizeIP: true,
+        },
       }),
     ],
   ],
